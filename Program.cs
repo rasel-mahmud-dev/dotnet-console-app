5d454435938950ea755b75ede1c68a8f0931e52c
rasel.mahmmud <rasel.mahmud.dev@gmail.com>
2025-01-18 14:43:46 +0600

    Initial dotnet console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloApp { 
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to your C# Console Application!");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}! How are you today?");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
